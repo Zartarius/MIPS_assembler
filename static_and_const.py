7f69d915acdab56158b100bca63b1b73c37b3798
Zartarius <zaryabayaz2@gmail.com>
2025-01-21 13:01:11 +1100

    Update static_and_const.py

--- static_and_const.py
+++ static_and_const.py
@@ -1,14 +1,17 @@
-'''
-This file parses the assembly file,
-looking for all static variables and constants, 
-returning them a key value pairs.
-'''
+def _valid_var_name(name) -> bool:
+    valid_chars = all(c.isalpha() or c == "_" or c.isdigit() for c in name)
+    return valid_chars and not name[0].isdigit()
 
-# Find all constants, and return as key value pairs
-def parse_for_constants(file) -> dict:
+def _parse_for_constants(lines) -> dict:
     constants = {}
-    for line in file:
+    for line in lines:
         if "=" in line:
             key, value = line.split("=")
             constants[key.strip()] = int(value)
-    print(constants)
+
+def _parse_for_globals(file) -> dict:
+    return None
+
+# Find all constants and statics (globals), and return as key value pairs
+def ret_static_and_const(lines) -> tuple[dict, dict]:
+    return (_parse_for_constants(lines), _parse_for_globals(lines))
