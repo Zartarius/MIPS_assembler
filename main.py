6ab8a8a9260a5e3225ca476f22b652bf5a36913e
Zartarius <zaryabayaz2@gmail.com>
2025-01-21 13:13:42 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import static_and_const #type: ignore
+import instructions #type: ignore
 
 # Exit if there any issues with the cl args or file
 if len(sys.argv) != 2:
@@ -9,8 +10,7 @@ elif not sys.argv[1].endswith(".s"):
     print("Error: Invalid file extension. Only .s files are supported.", file=sys.stderr)
     sys.exit(1)
 
-# Read the file and save in a list
-# Clean up the file too
+# Read the file and save in a list, after cleaning it up
 with open(sys.argv[1], "r") as file:
     lines = file.readlines()
     lines = [line.split("#", maxsplit=1)[0] for line in lines] # Remove comments
@@ -19,20 +19,7 @@ with open(sys.argv[1], "r") as file:
 
 constants, statics = static_and_const.ret_static_and_const(lines) # Parse all const and static variables
 
-prog_counter = 0
-# Search for main
-while True:
-    if prog_counter >= len(lines):
-        print("Error: No main label found in the file.", file=sys.stderr)
-        sys.exit(1)
-    elif "main:" in lines[prog_counter]:
-        prog_counter += 1
-        break
-    else:
-        prog_counter += 1
-        
+prog_counter = instructions.find_main(lines)
 
 while prog_counter < len(lines):
     pass
-
-sys.exit(0) # Marks end of program, technically not needed as we have reached the end anyways
