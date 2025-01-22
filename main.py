b7feb29adcfcde4164c2d0e1822c40af31319a0f
Zartarius <zaryabayaz2@gmail.com>
2025-01-22 17:05:17 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import sys
 import init #type: ignore
-from instructions import Instructions
+from program import Program #type: ignore
 
 
 lines = init.read_file(sys.argv)
 prog_counter = init.find_main(lines)
 variables = init.variables(lines) # Parse all labels and const and static variables
 
-instructions = Instructions(lines, prog_counter, variables)
+program = Program(lines, prog_counter, variables)
 
-while not instructions.halt():
-    instructions.execute()
+while program.halt() is False:
+    program.fetch_decode_execute()
